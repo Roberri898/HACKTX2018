82d8d0c7123c41e9d4b54e14b38012a8a9851b85
Roberto <roberri898@gmail.com>
2018-10-21 01:01:35 -0500

    MADE Login work

--- Website/User/Login.py
+++ Website/User/Login.py
@@ -15,14 +15,11 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-	r = requests.get('localhost:3030/user',params = {"email":request.form['username']})
+    r = requests.get('http://localhost:3030/user',params = {"email":request.form.get('username')})
+    print(r.status_code)
     if r.status_code == requests.codes.ok:
-        session['logged_in'] = True
         return "ok??????????????????"
     else:
-        flash('wrong password!')
+        return "asd"
     return home()
  
-if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4000)
