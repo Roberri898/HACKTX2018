25d68cd4c594f645dbb7800ed2cc0b8134100079
Alissa M. Flores <alissa.flores01@utrgv.edu>
2018-10-21 02:02:59 -0500

    login
    session now works

--- Website/User/Login.py
+++ Website/User/Login.py
@@ -1,25 +1,32 @@
 from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, escape, url_for
 import requests
 import os
  
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
  
 @app.route('/')
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return render_template('logged_in')
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return render_template("login.html")
 
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    r = requests.get('http://localhost:3030/user',params = {"email":request.form.get('username')})
-    print(r.status_code)
-    if r.status_code == requests.codes.ok:
-        return "ok??????????????????"
+    # r = requests.get('http://localhost:3030/user',params = {"email":request.form.get('username')})
+    # print(r.status_code)
+    if request.form['username']=='ok':
+        session['username'] = request.form['username']
+        return redirect(url_for('home'))
+
     else:
         return "asd"
     return home()
+
+@app.route('/logout')
+def logout():
+    session.pop('username',None)
+    return redirect(url_for('do_admin_login'))
  
