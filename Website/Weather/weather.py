939cb1acaa52c4528aaf4e9ab022ff5c08bddfd7
Tomas Torres <tomas3torres@live.com>
2018-10-21 03:02:04 -0500

    update weather

--- Website/Weather/weather.py
+++ Website/Weather/weather.py
@@ -1,5 +1,10 @@
 import requests
 import json
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
 
 api_key = 'eaf63414ac97fea73d11cdea989d87b8'
 city = 'McAllen'
@@ -24,23 +29,25 @@ def convertTemp(temp):
     f = temp *9/5 - 459.67
     c = temp - 273.15
 
-    return "{}F/{}C".format(int(f), int(c))
+    return "{}°F | {}°C".format(int(f), int(c))
 
-def getWeather(year, month, day):
+@app.route('/weather')
+def getWeather(year=2018, month=10, day=27):
     date = "{}-{}-{}".format(year, month, day)
     with requests.session() as sess:
         data = json.loads(sess.get(url).text)['list']
-
+        w_data = []
         for x in data:
             w_date = x['dt_txt']
             if date == w_date[:10]:
                 time = convertTime(w_date[11:-3])
                 description = x['weather'][0]['description']
                 temp = convertTemp(float(x['main']['temp']))
-                print(time)
-                print(description)
-                print(temp)
-                print('\n')
-            
+                t = (time, description, temp)
+                w_data.append(t)
 
-getWeather(2018,10,26)
+    show = False
+    if(len(w_data) == 0):
+        show = True
+    return render_template("weather.html", d=w_data, s = show)
+
